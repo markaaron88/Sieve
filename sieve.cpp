9b4819894ee9ae086d521136b1e95be54fb7e633
Mark Mariscal <markamariscal@gmail.com>
2020-01-14 20:21:32 -0800

    finished

--- sieve.cpp
+++ sieve.cpp
@@ -1,1 +1,52 @@
-//Title:
+//Title:Sieve.cpp
+//Abstract: Write a C++ program called sieve.cpp that implements the sieve of Eratosthenes algorithm 
+ //Author: Mark Mariscal
+ //ID: 7198
+ //Date: 01/14/2020
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(){
+
+    int *A; //ptr to hold value
+    int n;
+    cout << "Test case : ";
+    cin >> n;
+
+    A = new (nothrow) int[n + 1]; //number + 1 for array starts at 0
+    for(int p = 2; p <= n; p++){
+        A[p] = p;
+    }
+
+    int j;
+    for(int p = 2; (p * p) <= n; p++){
+        if(A[p] != 0){
+            j = p * p;
+            while (j <= n)
+            {
+                A[j] = 0;
+                j += p;
+                //copy the remaining elements of A to array L of the primes
+            }
+        }
+    }
+
+    cout << "Prime numbers are: ";
+
+    for (int i = 0; i <= n; i++)
+    {
+        if (A[i] != 0)
+        {
+
+            cout << A[i] << ",";
+        }
+    }
+    cout << "\n"; //new line
+    delete[] A;
+
+    return 0;
+}
